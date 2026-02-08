89eadb96fe41c73470d021f7164385ac17a0139e
kevin <liguori.km@gmail.com>
2026-02-08 18:10:34 -0500

    cleaning up after claude...big time

--- home/programs/dms/default.nix
+++ home/programs/dms/default.nix
@@ -2,17 +2,5 @@
 {
   programs.dank-material-shell = {
     enable = true;
-
-    enableClipboard = true;
-    enableSystemMonitoring = true;
-    enableNotifications = true;
-    enableLauncher = true;
-    enableScreenshot = true;
-    enableBluetooth = true;
-    enableNetwork = true;
-    enableAudio = true;
-    enableBrightness = true;
-    enablePolkit = true;
-    enableIdleInhibit = true;
   };
 }
