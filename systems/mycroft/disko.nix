1b35d253711864492439dcdff528b82476b891fe
kevin <liguori.km@gmail.com>
2026-02-19 09:49:03 -0500

    encrypted mycroft drives

--- systems/mycroft/disko.nix
+++ systems/mycroft/disko.nix
@@ -1,6 +1,5 @@
 {
   disk = {
-    # --- System disk ---
     system = {
       type = "disk";
       device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_500GB_S5H7NG0N214175Z";
@@ -8,45 +7,38 @@
         type = "gpt";
         partitions = {
           ESP = {
-            name = "ESP";
             size = "1G";
             type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [
-                "fmask=0022"
-                "dmask=0022"
-              ];
-              extraArgs = [
-                "-n"
-                "MYCBOOT"
-              ];
+              mountOptions = [ "umask=0077" ];
             };
           };
-
-          root = {
-            name = "root";
+          crypt = {
             size = "100%";
             content = {
-              type = "btrfs";
-              extraArgs = [
-                "-f"
-                "-L"
-                "mycsys"
-              ];
-              subvolumes = {
-                "@nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@persist" = {
-                  mountpoint = "/persist";
-                  mountOptions = [ "compress=zstd" ];
+              type = "luks";
+              name = "cryptroot";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
                 };
               };
             };
@@ -55,98 +47,117 @@
       };
     };
 
-    # --- Data disk ---
     data = {
       type = "disk";
       device = "/dev/disk/by-id/nvme-Samsung_SSD_990_EVO_Plus_4TB_S7U8NJ0Y622750V";
       content = {
         type = "gpt";
         partitions = {
-          data = {
-            name = "data";
+          crypt = {
             size = "100%";
             content = {
-              type = "btrfs";
-              extraArgs = [
-                "-f"
-                "-L"
-                "mycdata"
-              ];
-              subvolumes = {
-                # --- Media ---
-                "@movies" = {
-                  mountpoint = "/media/movies";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@tv" = {
-                  mountpoint = "/media/tv";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@music" = {
-                  mountpoint = "/media/music";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@audiobooks" = {
-                  mountpoint = "/media/audiobooks";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@books" = {
-                  mountpoint = "/media/books";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@games" = {
-                  mountpoint = "/media/games";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
+              type = "luks";
+              name = "cryptdata";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  # --- Media ---
+                  "@movies" = {
+                    mountpoint = "/media/movies";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@tv" = {
+                    mountpoint = "/media/tv";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@music" = {
+                    mountpoint = "/media/music";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@audiobooks" = {
+                    mountpoint = "/media/audiobooks";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@books" = {
+                    mountpoint = "/media/books";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@games" = {
+                    mountpoint = "/media/games";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
 
-                # --- Shares ---
-                "@shares-kevin" = {
-                  mountpoint = "/shares/kevin";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "@shares-wife" = {
-                  mountpoint = "/shares/wife";
-                  mountOptions = [ "compress=zstd" ];
-                };
+                  # --- Shares ---
+                  "@shares-kevin" = {
+                    mountpoint = "/shares/kevin";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@shares-jane" = {
+                    mountpoint = "/shares/jane";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
 
-                # --- Service data ---
-                "@paperless" = {
-                  mountpoint = "/data/paperless";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "@consume-kevin" = {
-                  mountpoint = "/data/paperless/consume/kevin";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "@consume-jane" = {
-                  mountpoint = "/data/paperless/consume/jane";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "@consume-household" = {
-                  mountpoint = "/data/paperless/consume/household";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "@postgresql" = {
-                  mountpoint = "/data/postgresql";
-                  mountOptions = [ "compress=zstd" ];
+                  # --- Service data ---
+                  "@paperless" = {
+                    mountpoint = "/data/paperless";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@consume-kevin" = {
+                    mountpoint = "/data/paperless/consume/kevin";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@consume-jane" = {
+                    mountpoint = "/data/paperless/consume/jane";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@consume-household" = {
+                    mountpoint = "/data/paperless/consume/household";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@postgresql" = {
+                    mountpoint = "/data/postgresql";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
                 };
               };
             };
