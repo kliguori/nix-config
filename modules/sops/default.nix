c89420b62739928c3e51a893b8448eac1c51dea8
kevin <liguori.km@gmail.com>
2026-02-20 23:51:15 -0500

    force sops to stop looking for ssh keys

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -4,10 +4,13 @@ let
   sopsDir = "/var/lib/sops-nix";
 in
 {
-  sops.age = {
-    keyFile = "${sopsDir}/key.txt";
-    generateKey = false;
-    sshKeyPaths = lib.mkForce [ ];
+  sops = {
+    age = {
+      keyFile = "${sopsDir}/key.txt";
+      generateKey = false;
+      sshKeyPaths = lib.mkForce [ ];
+    };
+    gnupg.sshKeyPaths = lib.mkForce [ ];
   };
 
   environment.persistence."/persist".directories = lib.mkIf cfg.impermanence.enable [
