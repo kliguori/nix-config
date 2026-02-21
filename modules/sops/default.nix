6da8d7e75663c5fa0fdb1cdcebc2e3b4e4bc695a
kevin <liguori.km@gmail.com>
2026-02-21 09:08:44 -0500

    Updated sops module to point at /persist on impermanent setups

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -1,19 +1,22 @@
 { config, lib, ... }:
 let
-  cfg = config.systemOptions;
-  sopsDir = "/var/lib/sops-nix";
+  persist = config.systemOptions.impermanence.enable;
+  baseDir = "/var/lib/sops-nix";
+
+  keyFile = 
+    if persist 
+    then "/persist${baseDir}/key.txt" 
+    else "${baseDir}/key.txt"; 
 in
 {
   sops = {
     age = {
-      keyFile = "${sopsDir}/key.txt";
+      inherit keyFile; 
       generateKey = false;
       sshKeyPaths = lib.mkForce [ ];
     };
     gnupg.sshKeyPaths = lib.mkForce [ ];
   };
 
-  environment.persistence."/persist".directories = lib.mkIf cfg.impermanence.enable [
-    sopsDir
-  ];
+  environment.persistence."/persist".directories = lib.mkIf persist [ baseDir ];
 }
